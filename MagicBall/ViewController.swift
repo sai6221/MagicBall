15a52f9d4805520125aee5c7c8bf3f01c0170f5b
Sai Reddy <sai6221@gmail.com>
2021-07-28 17:01:26 +0530

    Complete Magic Ball

--- MagicBall/ViewController.swift
+++ MagicBall/ViewController.swift
@@ -9,11 +9,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var ballImage: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
     }
-
-
+    
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        let imgs = [#imageLiteral(resourceName: "ball4"),#imageLiteral(resourceName: "ball2"),#imageLiteral(resourceName: "ball3"),#imageLiteral(resourceName: "ball5"),#imageLiteral(resourceName: "ball1")]
+        
+        ballImage.image = imgs [ Int.random(in: 0...4)]
+        
+    }
+    
 }
 
